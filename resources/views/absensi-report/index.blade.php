c914747c525a2fddfb206fdb9f44246cf22dd838
Inur123 <zainurroziqin38@gmail.com>
2025-05-19 04:27:50 +0700

    feat: Replace export icon with a new icon for better visual representation

--- resources/views/absensi-report/index.blade.php
+++ resources/views/absensi-report/index.blade.php
@@ -62,17 +62,14 @@
                         <div class="flex justify-start">
                             <a href="{{ route('absensi-report.export', $materi->id) }}"
                                 class="inline-flex items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" class="mr-2 h-4 w-4">
-                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                                    <polyline points="7 10 12 15 17 10" />
-                                    <line x1="12" x2="12" y1="15" y2="3" />
-                                </svg>
+
+                                <i class="ri-download-2-line mr-2 text-sm"></i>
+
                                 Export Data
                             </a>
                         </div>
                     </div>
+
                 </div>
             @endforeach
         </div>
